f4c681fc7deec38114023a7af95a1d45a2b3eae1
bahare <javidibaghban.b@gmail.com>
2012-10-01 13:54:02 +0330

    layout 0

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>civil-engineer</title>
+<link rel="stylesheet" type="text/css" href="reset.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<script type="text/javascript" src="zepto.js"></script>
+<script type="text/javascript" src="civil.js" ></script>
+</head>
+
+<body>
+
+<div class="container">
+	<div class="menu">
+    	<ul>
+        	<li><a href="#">صفحه اصلی</a></li>
+            <li><a href="#">درباره ما</a></li>
+            <li><a href="#">نمونه کارها</a></li>
+            <li><a href="#">تماس با ما</a></li>
+        </ul>
+    </div>
+    <div class="bg"></div>	
+</div>
+
+</body>
+</html>
